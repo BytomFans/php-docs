844cee36587898e8284e73b7b5ccdadd4adb350d
黄兴隆 <1292028314@qq.com>
2019-04-11 10:16:10 +0800

    build file

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -9,98 +9,96 @@
 // site configuration options.
 
 // List of projects/orgs using your project for the users page.
-const users = [
-  {
+const users = [{
     caption: 'User1',
     // You will need to prepend the image path with your baseUrl
     // if it is not '/', like: '/test-site/img/docusaurus.svg'.
     image: '/img/docusaurus.svg',
     infoLink: 'https://www.facebook.com',
     pinned: true,
-  },
-];
+}, ];
 
 const siteConfig = {
-  title: 'Bytom PHP API文档', // Title for your website.
-  tagline: 'A website for Bytom PHP SDK',
-  url: 'http://bytomphp.github.io', // Your website URL
-  baseUrl: '/', // Base URL for your project */
-  // For github.io type URLs, you would set the url and baseUrl like:
-  //   url: 'https://facebook.github.io',
-  //   baseUrl: '/test-site/',
+    title: 'Bytom PHP API文档', // Title for your website.
+    tagline: 'A website for Bytom PHP SDK',
+    url: 'https://bytomfans.github.io/php-docs/', // Your website URL
+    baseUrl: '/', // Base URL for your project */
+    // For github.io type URLs, you would set the url and baseUrl like:
+    //   url: 'https://facebook.github.io',
+    //   baseUrl: '/test-site/',
 
-  // Used for publishing and more
-  projectName: 'Bytom PHP SDK API',
-  organizationName: 'Bytom',
-  // For top-level user or org sites, the organization is still the same.
-  // e.g., for the https://JoelMarcey.github.io site, it would be set like...
-  //   organizationName: 'JoelMarcey'
+    // Used for publishing and more
+    projectName: 'Bytom PHP SDK API',
+    organizationName: 'Bytom',
+    // For top-level user or org sites, the organization is still the same.
+    // e.g., for the https://JoelMarcey.github.io site, it would be set like...
+    //   organizationName: 'JoelMarcey'
 
-  // For no header links in the top nav bar -> headerLinks: [],
-  headerLinks: [
-    {doc: 'key', label: 'Docs'},
-    // {doc: 'doc4', label: 'API'},
-    // {page: 'help', label: 'Help'},
-    // {blog: true, label: 'Blog'},
-  ],
-
-  // If you have users set above, you add it here:
-  users,
-
-  /* path to images for header/footer */
-  headerIcon: 'img/docusaurus.svg',
-  footerIcon: 'img/docusaurus.svg',
-  favicon: 'img/favicon.png',
-
-  /* Colors for website */
-  colors: {
-    primaryColor: '#2E8555',
-    secondaryColor: '#205C3B',
-  },
-
-  /* Custom fonts for website */
-  /*
-  fonts: {
-    myFont: [
-      "Times New Roman",
-      "Serif"
+    // For no header links in the top nav bar -> headerLinks: [],
+    headerLinks: [
+        { doc: 'key', label: 'Docs' },
+        // {doc: 'doc4', label: 'API'},
+        // {page: 'help', label: 'Help'},
+        // {blog: true, label: 'Blog'},
     ],
-    myOtherFont: [
-      "-apple-system",
-      "system-ui"
-    ]
-  },
-  */
 
-  // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
+    // If you have users set above, you add it here:
+    users,
 
-  highlight: {
-    // Highlight.js theme to use for syntax highlighting in code blocks.
-    theme: 'default',
-  },
+    /* path to images for header/footer */
+    headerIcon: 'img/docusaurus.svg',
+    footerIcon: 'img/docusaurus.svg',
+    favicon: 'img/favicon.png',
 
-  // Add custom scripts here that would be placed in <script> tags.
-  scripts: ['https://buttons.github.io/buttons.js'],
+    /* Colors for website */
+    colors: {
+        primaryColor: '#2E8555',
+        secondaryColor: '#205C3B',
+    },
 
-  // On page navigation for the current documentation page.
-  onPageNav: 'separate',
-  // No .html extensions for paths.
-  cleanUrl: true,
+    /* Custom fonts for website */
+    /*
+    fonts: {
+      myFont: [
+        "Times New Roman",
+        "Serif"
+      ],
+      myOtherFont: [
+        "-apple-system",
+        "system-ui"
+      ]
+    },
+    */
 
-  // Open Graph and Twitter card images.
-  ogImage: 'img/docusaurus.png',
-  twitterImage: 'img/docusaurus.png',
+    // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
+    copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
 
-  // Show documentation's last contributor's name.
-  // enableUpdateBy: true,
+    highlight: {
+        // Highlight.js theme to use for syntax highlighting in code blocks.
+        theme: 'default',
+    },
 
-  // Show documentation's last update time.
-  // enableUpdateTime: true,
+    // Add custom scripts here that would be placed in <script> tags.
+    scripts: ['https://buttons.github.io/buttons.js'],
 
-  // You may provide arbitrary config keys to be used as needed by your
-  // template. For example, if you need your repo's URL...
-  //   repoUrl: 'https://github.com/facebook/test-site',
+    // On page navigation for the current documentation page.
+    onPageNav: 'separate',
+    // No .html extensions for paths.
+    cleanUrl: true,
+
+    // Open Graph and Twitter card images.
+    ogImage: 'img/docusaurus.png',
+    twitterImage: 'img/docusaurus.png',
+
+    // Show documentation's last contributor's name.
+    // enableUpdateBy: true,
+
+    // Show documentation's last update time.
+    // enableUpdateTime: true,
+
+    // You may provide arbitrary config keys to be used as needed by your
+    // template. For example, if you need your repo's URL...
+    //   repoUrl: 'https://github.com/facebook/test-site',
 };
 
 module.exports = siteConfig;
