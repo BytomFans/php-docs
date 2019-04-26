ff5e979c4632a70ec87c1b5284427e21fdfd3968
黄兴隆 <1292028314@qq.com>
2019-04-26 10:31:59 +0800

    change oragin name

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -28,8 +28,8 @@ const siteConfig = {
     //   baseUrl: '/test-site/',
 
     // Used for publishing and more
-    projectName: 'Bytom PHP SDK API',
-    organizationName: 'Bytom',
+    projectName: 'php-docs',
+    organizationName: 'BytomFans',
     // For top-level user or org sites, the organization is still the same.
     // e.g., for the https://JoelMarcey.github.io site, it would be set like...
     //   organizationName: 'JoelMarcey'
